dea4e1294f92608ae45d2562cab1115a59fd414b
Alex Church <alex@church.id.au>
2020-12-01 18:55:04 +1100

    Solved day 1, part 1.

---
+++ puzzles/day01/puzzle.py
@@ -0,0 +1,20 @@
+import itertools
+
+from ..aoc import Puzzle
+
+
+class P(Puzzle):
+    
+    def process_input_item(self, input_item):
+        
+        return int(input_item)
+    
+    def _part1(self, input_data):
+        
+        for pair in itertools.combinations(input_data, 2):
+            if sum(pair) == 2020:
+                return pair[0] * pair[1]
+    
+    def _part2(self, input_data):
+        
+        pass
