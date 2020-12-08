3eecb0334176bf8ca85862a7c48e639731bbe880
Alex Church <alex@church.id.au>
2020-12-08 18:34:44 +1100

    Solved day 8, part 2.

--- puzzles/day08/puzzle.py
+++ puzzles/day08/puzzle.py
@@ -5,6 +5,8 @@ def run_program(input_data):
     
     acc = 0
     ptr = 0
+    complete = False
+    last_ptr = len(input_data) - 1
     seen_ptrs = set()
     
     while ptr not in seen_ptrs:
@@ -18,8 +20,12 @@ def run_program(input_data):
             ptr += value
         else:
             ptr += 1
+        
+        if ptr > last_ptr:
+            complete = True
+            break
     
-    return acc
+    return acc, complete
 
 
 class P(Puzzle):
@@ -33,8 +39,36 @@ class P(Puzzle):
     
     def _part1(self, input_data):
         
-        return run_program(input_data)
+        acc, complete = run_program(input_data)
+        
+        return acc
     
     def _part2(self, input_data):
         
-        pass
+        # Locate every "jmp" and "nop" instruction and swap them, one at a time,
+        # until the corrupted instruction is found. Nothing like a bit of brute
+        # force!
+        for i, item in enumerate(input_data):
+            instruction, value = item
+            
+            if instruction == 'acc':
+                # "No acc instructions were harmed in the corruption of this boot code"
+                continue
+            
+            # The instruction is either "jmp" or "nop". Take a copy of the
+            # instruction list and swap the instruction (jmp -> nop,
+            # nop -> jmp).
+            input_copy = input_data[:]
+            if instruction == 'jmp':
+                input_copy[i] = ('nop', value)
+            else:
+                input_copy[i] = ('jmp', value)
+            
+            # If the modified program terminates correctly, the corrupted
+            # instruction has been rectified
+            acc, complete = run_program(input_copy)
+            
+            if complete:
+                return acc
+        
+        return None
