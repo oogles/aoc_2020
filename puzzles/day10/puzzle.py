15e7bfca8c3085a84079be078e480ae32039f352
Alex Church <alex@church.id.au>
2020-12-13 07:18:12 +1100

    Solved day 10, part 2.

--- puzzles/day10/puzzle.py
+++ puzzles/day10/puzzle.py
@@ -28,4 +28,43 @@ class P(Puzzle):
     
     def _part2(self, input_data):
         
-        pass
+        # Add the device joltage (being the highest adapter joltage + 3) to the
+        # list so only chains that are valid right up to the device are counted
+        device_joltage = max(input_data) + 3
+        input_data.append(device_joltage)
+        
+        # Recursively traverse the list in sorted order, following every valid
+        # chain of adapters
+        input_data.sort()
+        
+        # Cache results of each index, as each one will be called A LOT
+        cache = {}
+        
+        def next(index, joltage):
+            
+            valid_chains = 0
+            
+            while True:
+                index += 1
+                next_joltage = input_data[index]
+                
+                if next_joltage > joltage + 3:
+                    break
+                
+                # It falls within the valid joltage range, so we've either
+                # reached the device (count a complete chain), or we need to
+                # proceed to the next adapter
+                if next_joltage == device_joltage:
+                    return 1
+                
+                try:
+                    result = cache[index]
+                except KeyError:
+                    result = next(index, next_joltage)
+                    cache[index] = result
+                
+                valid_chains += result
+            
+            return valid_chains
+        
+        return next(-1, 0)
