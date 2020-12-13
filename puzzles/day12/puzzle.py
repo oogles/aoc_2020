5d1ac74a52d9a1ed17b930701b2a8c39f7e89038
Alex Church <alex@church.id.au>
2020-12-13 19:39:18 +1100

    Solved day 12, part 2.

--- puzzles/day12/puzzle.py
+++ puzzles/day12/puzzle.py
@@ -55,4 +55,50 @@ class P(Puzzle):
     
     def _part2(self, input_data):
         
-        pass
+        ferry_x = 0
+        ferry_y = 0
+        
+        # The waypoint starts 10 units east and 1 unit north relative to the ferry
+        waypoint_x = 10
+        waypoint_y = 1
+        
+        for instruction, value in input_data:
+            if instruction in ('L', 'R'):
+                # Rotate the waypoint. Convert the value in degrees into the
+                # number of 90-degree increments it represents.
+                steps = value // 90
+                
+                multiplier = -1 if instruction == 'L' else 1
+                
+                for i in range(steps):
+                    x, y = waypoint_x, waypoint_y
+                    
+                    # The northward/southward coordinates become the
+                    # eastward/westward coordinates, depending on which way
+                    # the waypoint is being rotated
+                    if y >= 0:
+                        waypoint_x = y * multiplier
+                    else:
+                        waypoint_x = y * multiplier
+                    
+                    # The eastward/westward coordinates become the
+                    # northward/southward coordinates, depending on which way
+                    # the waypoint is being rotated
+                    if x >= 0:
+                        waypoint_y = x * multiplier * -1
+                    else:
+                        waypoint_y = x * multiplier * -1
+            elif instruction == 'F':
+                # Move the ferry to the waypoint the given number of times
+                ferry_x += (waypoint_x * value)
+                ferry_y += (waypoint_y * value)
+            elif instruction == 'N':
+                waypoint_y += value
+            elif instruction == 'E':
+                waypoint_x += value
+            elif instruction == 'S':
+                waypoint_y -= value
+            elif instruction == 'W':
+                waypoint_x -= value
+        
+        return abs(ferry_x) + abs(ferry_y)
