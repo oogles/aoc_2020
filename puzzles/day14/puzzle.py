37a7ca0c01622f743e0479fe94e2e1f4da5aa101
Alex Church <alex@church.id.au>
2020-12-15 19:23:07 +1100

    Solved day 14, part 2.

--- puzzles/day14/puzzle.py
+++ puzzles/day14/puzzle.py
@@ -35,4 +35,40 @@ class P(Puzzle):
     
     def _part2(self, input_data):
         
-        pass
+        mask = None
+        memory = defaultdict(lambda: 0)
+        
+        for instruction, value in input_data:
+            if instruction == 'mask':
+                mask = value
+            else:
+                mem_addr = instruction[4:-1]
+                
+                # Convert the memory address to a 0-padded 36-bit binary string,
+                # then convert that to a list to support item assignment
+                mem_addr = list(f'{int(mem_addr):0>36b}')
+                
+                # Apply the current bitmask, potentially expanding the single
+                # memory address into multiple
+                mem_addr_list = [mem_addr]
+                for i, bit in enumerate(mask):
+                    if bit == '1':
+                        # Replace the corresponding bit in ALL memory addresses
+                        # with 1
+                        for addr in mem_addr_list:
+                            addr[i] = '1'
+                    elif bit == 'X':
+                        # Replace the corresponding bit in ALL memory addresses
+                        # with 1 and, for each one, create a copy of the address
+                        # with the same bit set to 0
+                        for addr in mem_addr_list[:]:  # iterate a copy to facilitate appending to the list
+                            addr[i] = '1'
+                            addr_copy = addr[:]
+                            addr_copy[i] = '0'
+                            mem_addr_list.append(addr_copy)
+                
+                for addr in mem_addr_list:
+                    addr = int(''.join(addr), 2)
+                    memory[addr] = int(value)
+        
+        return sum(memory.values())
